4b789880fa5c232c2c131fa9c137aac35c8a906a
lumen nganje <lumennganje592@gmail.com>
2024-07-07 18:49:54 +0100

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/about'
+import Home from './pages/home'
+import Profile from './pages/profile'
+import SignIn from './pages/sign_in'
+import SignUp from './pages/sign_up'
 
 const App = () => {
   return (
-    <div className='bg-red-600'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/"  element={<Home/>} />
+        <Route path = "/about"  element={<About/>} />
+        <Route path = "/profile"  element={<Profile/>} />
+        <Route path = "/sign-in"  element={<SignIn/>} />
+        <Route path = "/sign-up"  element={<SignUp/>} />
+
+      </Routes>
+    </BrowserRouter>
+  
+    
   )
 }
 
